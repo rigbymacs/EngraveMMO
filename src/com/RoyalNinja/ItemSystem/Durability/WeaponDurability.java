b77bf0c69618d972d5ab32bc56a266c0b2cd68d4
Sanan Rao <ninjaboy323@hotmail.com>
2016-02-24 19:56:33 -0700

    Weapon durability first test added, currently no algorithm (Not sure if there will be in the future but just takes away 1 durability everytime you swing and hit someone, and it does more then 0 damage. Just a basic system.

--- src/com/RoyalNinja/ItemSystem/Durability/WeaponDurability.java
+++ src/com/RoyalNinja/ItemSystem/Durability/WeaponDurability.java
@@ -1,9 +1,45 @@
 package com.RoyalNinja.ItemSystem.Durability;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+
+import com.RoyalNinja.EngraveMMO.ItemGenerator.DurabilityUtil;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class WeaponDurability implements Listener {
 	
-	
+	@EventHandler
+	public void onWeaponLooseDura(EntityDamageByEntityEvent e) {
+		if (e.getDamager() instanceof Player) {
+			Player p = (Player) e.getDamager();
+			
+			if (p.getInventory().getItemInHand() != null) {
+				
+				ItemStack i = p.getInventory().getItemInHand();
+				
+				if (!(i.hasItemMeta() && i.getItemMeta().hasLore())) return;
+				
+				for (String s : i.getItemMeta().getLore()) {
+					if (s.contains(ChatColor.GOLD + "Durability:")) {
+						if (!e.isCancelled() && e.getDamage()>0) {
+							
+							DurabilityUtil du = new DurabilityUtil();
+							
+							Integer currentDura = du.getCurrentDurability(i);
+							
+							du.setDurability(i, currentDura--);
+							
+							return;
+							
+						}
+					}
+				}
+			}
+		}
+	}
 
 }
